503b50815789358daba83d6ee2617d26da1f443c
yogesdhakal <76492904+yogesdhakal@users.noreply.github.com>
2021-02-19 12:49:25 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ function addPagination(list){
          document.querySelector('.active').className = '';
          target.className = 'active';
 
-         showPage(target.textContent,data);
+         showPage(target.textContent,list);
       }
 
    })
